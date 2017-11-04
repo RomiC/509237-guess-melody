a5855350edc3e7ccbe91b6e4cf5799353cbfd914
Andrey Konopkov <konopkov@gmail.com>
2017-11-04 17:05:09 +0300

    fix: Поведение плеера - Запоминает загрузку и не отправляет повторные запросы

--- js/includes/player.js
+++ js/includes/player.js
@@ -1,7 +1,7 @@
 const playerWrapper = (id, src) => `
       <div class="player-wrapper">
         <div class="player">
-          <audio preload="auto" src="${src}" id="audio-${id}"></audio>
+          <audio preload="none" src="${src}" id="audio-${id}"></audio>
           <button class="player-control player-control--play"></button>
           <div class="player-track">
             <span class="player-status"></span>
@@ -10,7 +10,7 @@ const playerWrapper = (id, src) => `
       </div>`.trim();
 
 
-const updateClasses = (element, play = false) => {
+const updatePlayClasses = (element, play = false) => {
   if (play) {
     element.classList.remove(`player-control--play`);
     element.classList.add(`player-control--pause`);
@@ -20,14 +20,58 @@ const updateClasses = (element, play = false) => {
   }
 };
 
+const updateLoadingClasses = (element, loading = false, loaded = false) => {
+  if (loading) {
+    element.classList.add(`player-control--loading`);
+  } else {
+    element.classList.remove(`player-control--loading`);
+  }
+
+  if (loaded) {
+    element.classList.add(`player-control--loaded`);
+  } else {
+    element.classList.remove(`player-control--loaded`);
+  }
+};
+
 const fetchAudioAndPlay = (audio, button) => {
+  updatePlayClasses(button, true);
+  updateLoadingClasses(audio, true, false);
   fetch(audio.src)
       .then(() => {
-        return audio.play();
+        updateLoadingClasses(audio, false, true);
+        audioPlay(audio, button);
       })
       .catch(() => {
-        updateClasses(button, false);
+        updateLoadingClasses(audio, false, false);
+        updatePlayClasses(button, false);
       });
+};
+
+const audioPlay = (audio, button) => {
+  updatePlayClasses(button, true);
+  audio.play()
+      .catch(() => {
+        updateLoadingClasses(audio, false, false);
+        updatePlayClasses(button, false);
+      });
+};
+
+const audioPause = (audio, button) => {
+  audio.pause();
+  updatePlayClasses(button, false);
+};
+
+const isPlaying = (button) => {
+  return button.classList.contains(`player-control--pause`);
+};
+
+const isLoaded = (audio) => {
+  return audio.classList.contains(`player-control--loaded`);
+};
+
+const isLoading = (audio) => {
+  return audio.classList.contains(`player-control--loading`);
 };
 
 const playerHandler = (trigger, e, view) => {
@@ -43,18 +87,22 @@ const playerHandler = (trigger, e, view) => {
 
   audioElementsList.forEach((audio) => {
     const button = audio.nextElementSibling;
+    const trackIsPlaying = isPlaying(button);
+    const trackIsLoaded = isLoaded(audio);
+    const trackIsLoading = isLoading(audio);
 
     if (audio.id === selectedAudioElement.id) {
-      if (button.classList.contains(`player-control--play`)) {
-        fetchAudioAndPlay(audio, button);
-        updateClasses(button, true);
+      if (!trackIsPlaying) {
+        if (!trackIsLoaded || (!trackIsLoaded && !trackIsLoading)) {
+          fetchAudioAndPlay(audio, button);
+        } else {
+          audioPlay(audio, button);
+        }
       } else {
-        updateClasses(button, false);
-        audio.pause();
+        audioPause(audio, button);
       }
     } else {
-      audio.pause();
-      updateClasses(button, false);
+      audioPause(audio, button);
     }
   });
 };
